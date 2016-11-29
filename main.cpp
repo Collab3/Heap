4ff1803a53250d702c746f9aa8cc0fba209124f8
Charles Clayton <chaeclay@ut.utm.edu>
2016-11-29 12:44:44 -0600

    insert() and insert_helper() appear to be working properly. Print is working. Need to work on remove_max() and max().

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 int main() {
   heap h;
 
-  for(int i = 0; i < 10; i++){
+  for(int i=0; i<10; i++)
     h.insert(rand()%100);
-  }
 
   h.print();
+  
+  // while(h.size() > 0) {
+  //   cout << h.max() << endl;
+  //   h.remove_max();
+  // }
+  
+    //  h.print();
 
 return 0; 
 }
